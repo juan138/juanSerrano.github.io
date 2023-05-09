4f6310e00870479bf352f85ec1954ed08ff567db
juan138 <sejuancarlos4@gmail.com>
2023-05-09 09:19:52 -0600

    Delete portafolio.js

--- portafolio.js
+++
@@ -1,6 +0,0 @@
- //get-area-img.html
-
- document.getElementById('click-img').addEventListener("click", function(){ 
-     //alert("Hello World!"); 
-     window.location.href = "./demos-html/get-area-img.html";
-});
